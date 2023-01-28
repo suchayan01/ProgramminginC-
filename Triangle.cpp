c31186e7fb20850d7ae30a1754e555a7bf393d89
suchayan01 <suchayanbiswas@gmail.com>
2023-01-28 17:12:49 +0530

    Triangle

---
+++ Triangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float h, b;
+    cout<<"Enter height & breath";
+    cin>>h>>b;
+    cout<<"Area of the triangle "<<(h*b)*0.5<<endl;
+
+    return 0;
+}
---
+++ math.cpp
@@ -0,0 +1,8 @@
+#include<cmath>
+#include<iostream>
+using namespace std;
+int main(){
+    int c=sqrt(25)*3;
+    cout<<c;
+    return 0;
+}
